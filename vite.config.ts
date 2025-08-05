3f600ea56fc4dc0d87fead7b28d412bbe744d671
Kir Kostin <k.kostin@group-ib.com>
2025-08-05 18:18:15 +0300

    feat: update boilerplate config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import browserslistToEsbuild from 'browserslist-to-esbuild'
+
+const buildTarget = browserslistToEsbuild()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  build: {
+    target: buildTarget,
+    outDir: './dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: () => {
+          return `[name].[hash].[ext]`
+        },
+        entryFileNames: () => {
+          return `[name].[hash].js`
+        },
+      },
+    },
+  },
 })
